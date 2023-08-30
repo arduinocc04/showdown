025a5de7155ab4f818d2588ed77545eed9d8edfe
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2023-08-30 13:33:12 +0900

    multi handle

--- src/subParsers/makehtml/metadata.js
+++ src/subParsers/makehtml/metadata.js
@@ -4,7 +4,7 @@
 showdown.subParser('makehtml.metadata', function (text, options, globals) {
   'use strict';
 
-  text = text.replace(/!!([\wㄱ-ㅎ가-힣]+)!(\w+)!(NONE|https?:\/\/[\w\-\.\/]+)!!/, function (wholematch, handle, color, link) {
+  text = text.replaceAll(/!!([\wㄱ-ㅎ가-힣]+)!(\w+)!(NONE|https?:\/\/[\w\-\.\/]+)!!/, function (wholematch, handle, color, link) {
     globals.mentionedHandleInfos[handle] = {
       'color': color,
       'link': link
